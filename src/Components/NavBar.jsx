aefd1efae61e8bf25276ea94db194799a44df383
Ananth Suvvada <ananthsuvvada@gmail.com>
2026-05-19 11:48:20 +0530

    Add NavBar Feature

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,7 +1,28 @@
 import React from "react";
-
+import "../Styles/NavBar.css";
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <div className="NavBar">
+      <h1 className="logo">
+        BOY <span>2</span> MAN
+      </h1>
+      <ul className="menu">
+        <a href="">
+          <li>Home</li>
+        </a>
+        <a href="">
+          <li>Programs</li>
+        </a>
+        <a href="">
+          <li>Trainers</li>
+        </a>
+        <a href="">
+          <li>Contact</li>
+        </a>
+      </ul>
+      <button className="enrollBtn">Enroll Now</button>
+    </div>
+  );
 };
 
 export default NavBar;
