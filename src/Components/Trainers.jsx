69ecc7d29a1899ea10d5bcb4a5086d45dabbfb3f
Ananth Suvvada <ananthsuvvada@gmail.com>
2026-05-24 02:05:41 +0530

    Add Trainers Features

--- src/Components/Trainers.jsx
+++ src/Components/Trainers.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import "../Styles/Trainers.css";
+import { trainerData } from "../Data/Gym Website Data";
 
 const Trainers = () => {
-  return <div>Trainers</div>;
+  return (
+    <section className="trainers">
+      <h1>Our Trainers</h1>
+      <div className="trainersGrid">
+        {trainerData.map((trainer) => (
+          <div className="trainerCard" key={trainer.id}>
+            <img src={trainer.img} alt="" />
+            <h2>{trainer.name}</h2>
+            <h3>{trainer.role}</h3>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Trainers;
