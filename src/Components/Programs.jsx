4022c4f8f6e095e30848cacd15f6dcbdb52abbe7
Ananth Suvvada <ananthsuvvada@gmail.com>
2026-05-23 18:09:08 +0530

    Add responsiveness

--- src/Components/Programs.jsx
+++ src/Components/Programs.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "../Styles/Programs.css";
 
 const Programs = () => {
-  return <div>Programs</div>;
+  return (
+    <section className="programs">
+      <div className="programsContainer"></div>
+    </section>
+  );
 };
 
 export default Programs;
