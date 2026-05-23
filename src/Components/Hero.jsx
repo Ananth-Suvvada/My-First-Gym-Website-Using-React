61cad39157c3a1553e0e113f6cb7e081ceddfb0f
Ananth Suvvada <ananthsuvvada@gmail.com>
2026-05-23 17:38:35 +0530

    Add Hero Feature

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import "../Styles/Hero.css";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="hero">
+      <div className="hero-overlay">
+        <div className="hero-content">
+          <h1 className="hero-title">
+            BECOME A <span>MAN</span>
+          </h1>
+          <h3>
+            By choosing <span>BOY 2 MAN</span> GYM
+          </h3>
+          <p>
+            Kill <span> THE BOY </span> & <span> REBORN </span>& Let the world
+            see the <span> BEAST </span>
+            inside you.
+          </p>
+
+          <div className="hero-btns">
+            <button className="joinBtn">Join Us</button>
+            <button className="vipBtn">VIP Membership</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
