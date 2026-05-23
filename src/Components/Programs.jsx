5e173994ee80781464dcfe079b3e81f7a567a888
Ananth Suvvada <ananthsuvvada@gmail.com>
2026-05-23 22:40:10 +0530

    Add Programs Features

--- src/Components/Programs.jsx
+++ src/Components/Programs.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import "../Styles/Programs.css";
+import { programData } from "../Data/Gym Website Data.js";
 
 const Programs = () => {
   return (
     <section className="programs">
-      <div className="programsContainer"></div>
+      <h1>Our Programmes</h1>
+      <div className="programsGrid">
+        {programData.map((program) => (
+          <div id="specialCard" className="programeCard" key={program.id}>
+            <img src={program.img} alt="" />
+            <h2>{program.title}</h2>
+            <p>{program.description}</p>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
